1eac6d3f773d9072ddfbe6b20b39becad7961c2d
Josh Solis <joshsolis@joshs-mbp.attlocal.net>
2019-06-18 16:43:11 -0500

    finished ancestor.py somehow

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,13 +1,17 @@
 def earliest_ancestor(family_list, child):
     vertices = {}
     for (v1, v2) in family_list:
-        vertices[v1] = set()
-        vertices[v1].add(v2)
+        if v2 in vertices:
+            pass
+        else:
+            vertices[v2] = set()
+        vertices[v2].add(v1)
     
     visited = set()
     q = []
     q.append([child])
     # While the queue is not empty...
+    path_copy = list()
     while len(q) > 0:
         # Dequeue the first PATH
         p = q.pop(0)
@@ -18,16 +22,23 @@ def earliest_ancestor(family_list, child):
             # Mark it as visited
             visited.add(v)
             # Then add A PATH TO all of its neighbors to the back of the queue
-            for neighbor in vertices[v]:
-                # Copy the path
-                path_copy = [p]
-                # Append the neighbor to the back of the copy
-                # Enqueue copy
-                path_copy.append(neighbor)
-                q.append(path_copy)
-    print(path_copy)
+            if v in vertices:
+
+                for neighbor in vertices[v]:
+                    # Copy the path
+                    path_copy = list(p)
+                    # Append the neighbor to the back of the copy
+                    # Enqueue copy
+                    path_copy.append(neighbor)
+                    q.append(path_copy)
     print(vertices)
-    pass
+
+    if path_copy:
+        return path_copy[-1]
+    else:
+        return -1
+    #return path_copy[-1]
+
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
