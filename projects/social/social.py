c92ff567bceee372c30fa4163f9870a41fe13c17
Josh Solis <joshsolis@joshs-mbp.attlocal.net>
2019-06-21 09:17:07 -0500

    finished sc

--- projects/social/social.py
+++ projects/social/social.py
@@ -113,8 +113,8 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print("Friendships")
+    sg.populateGraph(10, 4)
+    print("Friendships:")
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print("Connections:")
